951b96a4f436b26b9da15c7d36a1b7ab691ef68a
rosi-zh <114585722+rosi-zh@users.noreply.github.com>
2024-02-11 14:11:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.90.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragerozhe"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
